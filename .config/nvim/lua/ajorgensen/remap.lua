ebbf4310db04feb28203b227d9566a24be050659
Andrew Jorgensen <andrew@andrewjorgensen.com>
2022-10-06 10:10:44 -0400

    Include hidden files in telescope search

--- .config/nvim/lua/ajorgensen/remap.lua
+++ .config/nvim/lua/ajorgensen/remap.lua
@@ -15,7 +15,7 @@ nnoremap("<c-l>", "<c-w>l")
 --nnoremap("<leader>p", ":GFiles<cr>")
 
 -- Telescope
-nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
+nnoremap("<leader>ff", "<cmd>Telescope find_files hidden=true<cr>")
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<cr>")
 nnoremap("<leader>fb", "<cmd>Telescope buffers<cr>")
 nnoremap("<leader>fh", "<cmd>Telescope help_tags<cr>")
