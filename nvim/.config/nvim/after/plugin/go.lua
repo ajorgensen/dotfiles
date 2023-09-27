89134ea746f4004e6ca5d7248d7b03d09b36ac0f
Andrew Jorgensen <andrew@andrewjorgensen.com>
2023-09-27 14:36:36 -0400

    color scheme and git rebase-onto

--- nvim/.config/nvim/after/plugin/go.lua
+++ nvim/.config/nvim/after/plugin/go.lua
@@ -1,10 +1,12 @@
 require("go").setup()
 
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*.go",
-    callback = function()
-        require('go.format').goimport()
-    end,
-    group = format_sync_grp,
-})
+vim.keymap.set("n", "<leader>gi", "<cmd> GoImport<CR>", {})
+
+--local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--    pattern = "*.go",
+--    callback = function()
+--        require('go.format').goimport()
+--    end,
+--    group = format_sync_grp,
+--})
