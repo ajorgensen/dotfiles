d90c0af67461767a6b439cdcdb0a7f97d2ae3381
Andrew Jorgensen <andrew@andrewjorgensen.com>
2023-12-05 11:14:28 -0500

    Fix issue with leader not being set before lazy was initialized

--- nvim/.config/nvim/after/plugin/snippets.lua
+++ nvim/.config/nvim/after/plugin/snippets.lua
@@ -1,1 +1,8 @@
 require("luasnip.loaders.from_snipmate").lazy_load()
+
+function OpenSnippetsFile()
+    local filepath = "~/.config/nvim/snippets/" .. vim.bo.filetype .. '.snippets'
+    vim.api.nvim_command("edit" .. filepath)
+end
+
+vim.keymap.set("n", "<leader>os", ':lua OpenSnippetsFile()<cr>', opts)
--- nvim/.config/nvim/lua/ajorgensen/init.lua
+++ nvim/.config/nvim/lua/ajorgensen/init.lua
@@ -1,19 +1,5 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("ajorgensen.lazy")
-require("ajorgensen.set")
 require("ajorgensen.remap")
+require("ajorgensen.set")
 require("ajorgensen.autocmd")
 
 local augroup = vim.api.nvim_create_augroup
@@ -43,3 +29,18 @@ autocmd({ 'BufWritePre' }, {
     pattern = '*',
     command = [[%s/\s\+$//e]],
 })
+
+-- Configure Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+require("ajorgensen.lazy")
