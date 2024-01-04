04c8a95b7355b83878e8b3d7154c9ab622cf8197
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-01-04 16:55:24 -0500

    Silence change detection

--- nvim/.config/nvim/lua/ajorgensen/lazy_init.lua
+++ nvim/.config/nvim/lua/ajorgensen/lazy_init.lua
@@ -11,4 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("ajorgensen.lazy")
+require("lazy").setup({ 
+    spec = "ajorgensen.lazy", 
+    change_detetion = { 
+        notify = false
+    }
+})
