df49836fe7cbbda8f18c74fea8c1d67f373d85e1
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-01-09 21:50:23 -0500

    Fix: missing dependency for ray-x/go.nvim

--- nvim/.config/nvim/lua/ajorgensen/lazy/go.lua
+++ nvim/.config/nvim/lua/ajorgensen/lazy/go.lua
@@ -1,6 +1,9 @@
 return {
     {
         'ray-x/go.nvim',
+        dependencies = {
+            'ray-x/guihua.lua',
+        },
         config = function()
             require('go').setup()
             vim.keymap.set("n", "<leader>gi", "<cmd> GoImport<CR>", {})
