fd03175046e5dfd07195270a5c5424e47daa6ac5
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-01-17 14:43:29 -0500

    feat: alpha and some snippets stuff

--- nvim/.config/nvim/lua/ajorgensen/lazy/snippets.lua
+++ nvim/.config/nvim/lua/ajorgensen/lazy/snippets.lua
@@ -1,19 +1,40 @@
 return {
-    {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lsp",
         "L3MON4D3/LuaSnip",
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-        config = function() 
-            require("luasnip.loaders.from_snipmate").lazy_load()
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
+    },
+    config = function()
+        -- Set up nvim-cmp.
+        local cmp = require'cmp'
+        require('luasnip.loaders.from_vscode').lazy_load()
 
-            function OpenSnippetsFile()
-                local filepath = "~/.config/nvim/snippets/" .. vim.bo.filetype .. '.snippets'
-                vim.api.nvim_command("edit" .. filepath)
-            end
-
-            vim.keymap.set("n", "<leader>os", ':lua OpenSnippetsFile()<cr>', opts)
-        end
-    }
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, 
+            }, {
+                    { name = 'buffer' },
+                })
+        })
+    end,
 }
