50e7516ab22cc0efdb45564d7562a8bc86e53701
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-01-25 12:41:31 -0500

    feat: use live-grep with args

--- nvim/.config/nvim/lua/ajorgensen/lazy/telescope.lua
+++ nvim/.config/nvim/lua/ajorgensen/lazy/telescope.lua
@@ -3,7 +3,10 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.4',
         dependencies = { 
-            { 'nvim-lua/plenary.nvim' },
+            { 
+                'nvim-lua/plenary.nvim',
+                'nvim-telescope/telescope-live-grep-args.nvim',
+            },
         },
         config = function()
             require('telescope').setup({
@@ -25,10 +28,11 @@ return {
             })
 
             local builtin = require('telescope.builtin')
+            local extensions = require('telescope').extensions
             vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden = true }) end, {})
             vim.keymap.set('n', '<leader>fm', function() builtin.marks() end, {})
             vim.keymap.set('n', '<leader>fb', function() builtin.buffers() end, {})
-            vim.keymap.set('n', '<leader>fg', function() builtin.live_grep() end, {})
+            vim.keymap.set('n', '<leader>fg', function() extensions.live_grep_args.live_grep_args() end, {})
             vim.keymap.set('n', 'C-p', builtin.git_files, {})
             vim.keymap.set('n', '<leader>ps', function()
                 builtin.grep_string({ search = vim.fn.input("Grep > ") })
