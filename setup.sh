eec8ef36542e18ddcbe1a1a5edf49c50a0c80fe0
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-02-06 18:03:58 -0500

    fix: alacritty themes and add rectangle config

--- setup.sh
+++ setup.sh
@@ -8,3 +8,9 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # link all the things
 ln -sf "$PWD/alacritty" "$XDG_CONFIG_HOME"
 ln -sf "$PWD/tmux" "$XDG_CONFIG_HOME"
+
+
+# ~~~~~ OSX related configuration ~~~~~
+if [ "$(uname -s)" == "Darwin" ]; then
+    ln -sf "$PWD/Rectangle" "$HOME/Library/Application Support"
+fi
