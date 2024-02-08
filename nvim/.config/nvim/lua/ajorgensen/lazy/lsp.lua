832f44d605aceecdf0dc5da0bfa6089fb3114846
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-02-07 21:05:15 -0500

    feat[lsp]: add shortkey to go to implementations

--- nvim/.config/nvim/lua/ajorgensen/lazy/lsp.lua
+++ nvim/.config/nvim/lua/ajorgensen/lazy/lsp.lua
@@ -94,6 +94,7 @@ return {
 
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
                 vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+                vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                 vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
