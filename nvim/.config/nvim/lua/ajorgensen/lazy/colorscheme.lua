c9fe28f13411a85436258da91639f3c3f55a6976
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-03-14 15:28:01 -0400

    random changes

--- nvim/.config/nvim/lua/ajorgensen/lazy/colorscheme.lua
+++ nvim/.config/nvim/lua/ajorgensen/lazy/colorscheme.lua
@@ -3,13 +3,5 @@ return {
     'ellisonleao/gruvbox.nvim',
     'rose-pine/neovim',
     'catppuccin/nvim',
-    {
-        'folke/tokyonight.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function() 
-            vim.cmd([[colorscheme tokyonight]])
-        end,
-    }
+    'folke/tokyonight.nvim',
 }
-
