6520f1c8724de09417c5abd733b691850bb61213
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-03-14 15:34:56 -0400

    task: move nvim and add explicit link

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ export XDG_CONFIG_HOME="$HOME/.config"
 # link all the things
 ln -sf "$PWD/alacritty" "$XDG_CONFIG_HOME"
 ln -sf "$PWD/tmux" "$XDG_CONFIG_HOME"
-
+ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME"
 
 # ~~~~~ OSX related configuration ~~~~~
 if [ "$(uname -s)" == "Darwin" ]; then
