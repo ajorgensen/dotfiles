9847dafa563a089cc9eb9a0d178af4ed701a5f21
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-03-15 12:55:54 -0400

    fix: remove duplicate lsp-zero on attach

--- nvim/lua/ajorgensen/lazy/lsp.lua
+++ nvim/lua/ajorgensen/lazy/lsp.lua
@@ -9,6 +9,42 @@ return {
             vim.g.lsp_zero_extend_cmp = 0
             vim.g.lsp_zero_extend_lspconfig = 0
         end,
+        config = function()
+            local lsp = require("lsp-zero")
+
+            lsp.set_preferences({
+                suggest_lsp_servers = false,
+                sign_icons = {
+                    error = 'E',
+                    warn = 'W',
+                    hint = 'H',
+                    info = 'I'
+                }
+            })
+
+            lsp.on_attach(function(client, bufnr)
+                local opts = { buffer = bufnr, remap = false }
+
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+                vim.keymap.set("n", "<leader>vmt", function() vim.lsp.buf.format() end, opts)
+            end)
+
+            lsp.setup()
+
+            vim.diagnostic.config({
+                virtual_text = true
+            })
+        end
     },
     {
         'williamboman/mason.nvim',
@@ -72,14 +108,6 @@ return {
             -- This is where all the LSP shenanigans will live
             local lsp_zero = require('lsp-zero')
             lsp_zero.extend_lspconfig()
-
-            --- if you want to know more about lsp-zero and mason.nvim
-            --- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-            lsp_zero.on_attach(function(client, bufnr)
-                -- see :help lsp-zero-keybindings
-                -- to learn the available actions
-                lsp_zero.default_keymaps({ buffer = bufnr })
-            end)
 
             require('mason-lspconfig').setup({
                 ensure_installed = {},
