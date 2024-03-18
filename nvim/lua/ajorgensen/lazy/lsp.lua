c3afbaea5997658dce8bb672f2ba7fb22ee16add
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-03-18 10:53:17 -0400

    feat: auto-import golang

--- nvim/lua/ajorgensen/lazy/lsp.lua
+++ nvim/lua/ajorgensen/lazy/lsp.lua
@@ -44,6 +44,14 @@ return {
             vim.diagnostic.config({
                 virtual_text = true
             })
+
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                pattern = '*.go',
+                callback = function()
+                    vim.lsp.buf.format()
+                    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+                end
+            })
         end
     },
     {
