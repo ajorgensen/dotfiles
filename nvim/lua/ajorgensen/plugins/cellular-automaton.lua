a2019cc17eb9db53a9924e177c22ba0b6f327899
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-04-11 15:26:41 -0400

    feat: reformatting

---
+++ nvim/lua/ajorgensen/plugins/cellular-automaton.lua
@@ -0,0 +1,1 @@
+return { 'Eandrju/cellular-automaton.nvim' }
--- nvim/lua/ajorgensen/plugins/chatgpt.lua
+++ nvim/lua/ajorgensen/plugins/chatgpt.lua
@@ -1,13 +1,11 @@
 return {
-    {
-        "jackMort/ChatGPT.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            require("chatgpt").setup({})
-        end,
-    }
+	"jackMort/ChatGPT.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		require("chatgpt").setup({})
+	end,
 }
