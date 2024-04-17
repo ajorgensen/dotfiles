276594ecb6b55a9b6f617559dc5f512dc50abbb4
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-04-17 09:19:46 -0400

    task: autocmd and keymaps

--- nvim/lua/ajorgensen/keymaps.lua
+++ nvim/lua/ajorgensen/keymaps.lua
@@ -1,4 +1,6 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR><cmd>lua vim.diagnostic.reset()<CR>",
+    { desc = "Restart LSP server" })
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
