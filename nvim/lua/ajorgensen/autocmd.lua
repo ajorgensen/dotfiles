16fd90bcbf4823a528972da69ce26f82fca4a4ad
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-04-17 09:19:46 -0400

    task: autocmd and keymaps

--- nvim/lua/ajorgensen/autocmd.lua
+++ nvim/lua/ajorgensen/autocmd.lua
@@ -68,10 +68,9 @@ autocmd('BufNewFile', {
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
-
