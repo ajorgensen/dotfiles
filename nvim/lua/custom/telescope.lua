99d93845b3b7434df949634290ef78a88784269e
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-05-20 17:35:25 -0400

    task: add harpoon to telescope

--- nvim/lua/custom/telescope.lua
+++ nvim/lua/custom/telescope.lua
@@ -1,4 +1,8 @@
-require("telescope").setup {
+require("telescope").setup({
+  defaults = {
+    path_display = { "truncate" },
+    file_ignore_patterns = { "^.git/", "^node_modules/", "^vendor/" },
+  },
   extensions = {
     fzf = {},
     wrap_results = true,
@@ -6,21 +10,33 @@ require("telescope").setup {
       limit = 100,
     },
   },
-}
+})
 
 pcall(require("telescope").load_extension, "fzf")
 pcall(require("telescope").load_extension, "smart_history")
 pcall(require("telescope").load_extension, "live_grep_args")
+pcall(require("telescope").load_extension, "harpoon")
 
-local builtin = require "telescope.builtin"
-local extensions = require('telescope').extensions
+local builtin = require("telescope.builtin")
+local extensions = require("telescope").extensions
 
-vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden = true }) end, {})
-vim.keymap.set('n', '<leader>fm', function() builtin.marks() end, {})
-vim.keymap.set('n', '<leader>fb', function() builtin.buffers() end, {})
-vim.keymap.set('n', '<leader>fg', function() extensions.live_grep_args.live_grep_args() end, {})
-vim.keymap.set('n', 'C-p', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set("n", "<leader>ff", function()
+  builtin.find_files({ hidden = true })
+end, {})
+vim.keymap.set("n", "<leader>fh", function()
+  extensions.harpoon.marks()
+end, {})
+vim.keymap.set("n", "<leader>fm", function()
+  builtin.marks()
+end, {})
+vim.keymap.set("n", "<leader>fb", function()
+  builtin.buffers()
+end, {})
+vim.keymap.set("n", "<leader>fg", function()
+  extensions.live_grep_args.live_grep_args()
+end, {})
+vim.keymap.set("n", "C-p", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>sm', ":Telescope harpoon marks<CR>", { desc = "Harpoon [M]arks" })
+vim.keymap.set("n", "<leader>sm", ":Telescope harpoon marks<CR>", { desc = "Harpoon [M]arks" })
