c9b4821dceac5561d5f19be154ddf5abca109e41
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-05-21 11:23:08 -0400

    task: treesitter and remove supermaven

--- nvim/lua/custom/plugins/ai.lua
+++ nvim/lua/custom/plugins/ai.lua
@@ -1,10 +1,4 @@
 return {
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({})
-    end,
-  },
-  'David-Kunz/gen.nvim',
-  -- "github/copilot.vim",
+  "David-Kunz/gen.nvim",
+  "github/copilot.vim",
 }
