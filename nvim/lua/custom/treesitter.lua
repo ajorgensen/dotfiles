e67534578f2a5c52c8f0f63d8cfea01a20ffe450
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-05-21 11:23:08 -0400

    task: treesitter and remove supermaven

---
+++ nvim/lua/custom/treesitter.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.setup = function()
+  local group = vim.api.nvim_create_augroup("custom-treesitter", { clear = true })
+
+  require("nvim-treesitter").setup({
+    ensure_install = "community",
+  })
+
+  local syntax_on = {}
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+      local bufnr = args.buf
+      local ft = vim.bo[bufnr].filetype
+      pcall(vim.treesitter.start)
+
+      if syntax_on[ft] then
+        vim.bo[bufnr].syntax = "on"
+      end
+    end,
+  })
+end
+
+M.setup()
+
+return M
