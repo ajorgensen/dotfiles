a9cc8b4c8b647f1d4bc3a975b53df64a304f449d
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-08-01 21:32:29 -0400

    fzf-lua

--- .config/nvim/lua/custom/plugins/chatgpt.lua
+++ .config/nvim/lua/custom/plugins/chatgpt.lua
@@ -1,11 +1,13 @@
 return {
-	"jackMort/ChatGPT.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		require("chatgpt").setup({})
-	end,
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    require("chatgpt").setup {}
+  end,
 }
