ea157a3a15ec2c268d3c9168c79e3957c988a3c5
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-09-03 21:31:25 -0400

    feat: add nvim-treesitter-context

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,8 +1,11 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  lazy = false,
-  config = function()
-    require("custom.treesitter").setup()
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+      require("custom.treesitter").setup()
+    end,
+  },
+  { "nvim-treesitter/nvim-treesitter-context" },
 }
