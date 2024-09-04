180fea228143c83368fdd68959645f58e17b9924
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-09-03 22:22:21 -0400

    avante

--- .config/nvim/lua/custom/plugins/ai.lua
+++ .config/nvim/lua/custom/plugins/ai.lua
@@ -6,4 +6,50 @@ return {
       require("supermaven-nvim").setup {}
     end,
   },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    opts = {
+      -- add any opts here
+    },
+    -- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
+    -- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
+    build = ":AvanteBuild source=false",
+    config = function()
+      require "custom.ai"
+    end,
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
