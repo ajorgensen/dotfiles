7e1f306dafef1b4e0cff6a301c2713d425fc401b
Andrew Jorgensen <andrew@andrewjorgensen.com>
2024-09-10 10:51:30 -0400

    task: oil configs

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -3,16 +3,26 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup({
+      require("oil").setup {
         columns = { "icon" },
         keymaps = {
           ["<C-h>"] = false,
           ["<M-h>"] = "actions.select_split",
         },
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
         view_options = {
           show_hidden = true,
+          natural_order = true,
+          is_always_hidden = function(name, _)
+            return name == ".." or name == ".git"
+          end,
         },
-      })
+        win_options = {
+          wrap = true,
+        },
+      }
 
       -- Open parent directory in current window
       vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
