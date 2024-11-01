017d7eb023df9c5358d1040c52f61e8d53cc5afc
Andrew Jorgensen <git@aj.codes>
2024-11-01 10:07:15 -0400

    feat(hammerspoon): add mute toggle to menubar

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -67,7 +67,7 @@ hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "S", function()
 
   -- Calculate the new size (90% of screen size)
   local newWidth = screenFrame.w * 0.55
-  local newHeight = screenFrame.h * 0.75
+  local newHeight = screenFrame.h * 0.95
 
   -- Calculate the new position (centered)
   local newX = screenFrame.x + (screenFrame.w - newWidth) / 2
@@ -139,3 +139,28 @@ hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "H", function()
     win:setFrame(f)
   end
 end)
+
+-- Microphone mute toggle and menu bar icon
+local micMuter = hs.menubar.new()
+
+local function setMicMuteIcon(isMuted)
+  if isMuted then
+    micMuter:setTitle("🔇")
+  else
+    micMuter:setTitle("🔊")
+  end
+end
+
+local function toggleMicMute()
+  local currentAudioInput = hs.audiodevice.defaultInputDevice()
+  local isMuted = currentAudioInput:inputMuted()
+  currentAudioInput:setInputMuted(not isMuted)
+  setMicMuteIcon(not isMuted)
+end
+
+if micMuter then
+  micMuter:setClickCallback(toggleMicMute)
+  setMicMuteIcon(hs.audiodevice.defaultInputDevice():inputMuted())
+end
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "M", toggleMicMute)
