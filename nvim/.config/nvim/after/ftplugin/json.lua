169501917dfe52a9072d38fee3942e63561f454c
Andrew Jorgensen <git@aj.codes>
2025-01-06 16:07:01 -0500

    stuff and things

--- nvim/.config/nvim/after/ftplugin/json.lua
+++ nvim/.config/nvim/after/ftplugin/json.lua
@@ -4,5 +4,5 @@ setlocal.foldmethod = "expr"
 setlocal.foldexpr = "nvim_treesitter#foldexpr()"
 setlocal.foldlevel = 99
 
-setlocal.opt.shiftwidth = 2
-setlocal.opt.expandtab = true
+setlocal.shiftwidth = 2
+setlocal.expandtab = true
