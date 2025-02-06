7c31154de85828e49e319097cbc7150c0c24753a
Andrew Jorgensen <git@aj.codes>
2025-02-06 14:08:14 -0500

    task: colors

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
   "RRethy/base16-nvim",
+  "folke/tokyonight.nvim",
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd "colorscheme base16-onedark"
+        vim.cmd "colorscheme tokyonight-night"
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
