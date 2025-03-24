eee67cb5460c1c2090b4986aa669630773876b50
Andrew Jorgensen <aj@digits.com>
2025-03-24 12:57:26 -0400

    feat: wezterm rename tab

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,25 +9,25 @@ config.keys = {
     mods = "CTRL|SHIFT",
     action = act.ClearScrollback("ScrollbackAndViewport"),
   },
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
 }
 
 -- Colors
-local function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return "Dark"
-end
-
-local function colorscheme(appearance)
-  if appearance:find("Dark") then
-    return "Tokyo Night"
-  else
-    return "Github (base16)"
-  end
-end
-
-config.color_scheme = colorscheme(get_appearance())
+config.color_scheme = "Tokyo Night"
 
 -- Fonts
 config.font = wezterm.font({ family = "SauceCodePro Nerd Font", weight = "Regular" })
