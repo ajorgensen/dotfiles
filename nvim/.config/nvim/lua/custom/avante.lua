7673dbb7e719bbf44e194450712e25e30a3c5199
Andrew Jorgensen <aj@digits.com>
2025-03-26 22:02:57 -0400

    task: avante update

--- nvim/.config/nvim/lua/custom/avante.lua
+++ nvim/.config/nvim/lua/custom/avante.lua
@@ -4,12 +4,19 @@ require("avante").setup({
     model = "gemini-2.0-flash",
   },
   claude = {
-    model = "claude-3-7-sonnet-20250219",
+    model = "claude-3-5-sonnet-latest",
+  },
+  openai = {
+    model = "o3-mini",
   },
   vendors = {
     ["gemini-pro"] = {
       __inherited_from = "gemini",
       model = "gemini-2.5-pro-exp-03-25",
+    },
+    ["claude-37"] = {
+      __inherited_from = "claude",
+      model = "claude-3-7-sonnet-latest",
     },
   },
 })
